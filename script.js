d54d643447af990881ded13d037e3f426f2d98ef
Khris Lorenz <khrislorenz@Khriss-MacBook-Pro.local>
2022-08-22 04:11:05 -0700

    foundations

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
 // Assignment code here
 // create variables to hold the character options for Special, Lowercase, Uppercase, and Numeric Charcter.
-
-// create an empty array that will hold the users selected characters.
-// var possibleChars = []
-// var passWord Array = []
-
-//o game start prompt for users of charcter and hold in a variable
-// var numChars = prompt("How many charchters would you like in your password")
-
-// check the users and confirm they put in a number between 8 and 128, and it is numeric.
-
-// write funcrion that validates users input
-
-// function validateUserInput(chars) {
-  // parseInt(chars)
-// if ( chars < 8 || chars > 128) {
-// return null
-//}
-//}
-
-// after validation vreate variables for charcter selections.
-
 // var specialNums = confirm("Would you like special characters")
 // var numericallNums = confirm("Would you like special characters")
 // var lowerChars = confirm("Would you like special characters")
 // var upperChars = confirm("Would you like special characters")
 
+// create an empty array that will hold the users selected characters.
+var possibleChars = []
+var passwordArray = []
+
+//o game start prompt for users of charcter and hold in a variable
+var numChars = prompt("How many charchters would you like in your password")
+
+// check the users and confirm they put in a number between 8 and 128, and it is numeric.
+// write funcrion that validates users input
+
+function validateUserInput(chars) {
+  parseInt(chars)
+if ( chars < 8 || chars > 128) {
+return null
+}
+}
+
+// after validation vreate variables for charcter selections.
+
+var specialNums = confirm("Would you like special characters")
+var numericallNums = confirm("Would you like special characters")
+var lowerChars = confirm("Would you like special characters")
+var upperChars = confirm("Would you like special characters")
+
 // validate that at least one option was selected
 
-// if (!specialChars && !numericalNums && !lowerChars && !upperChars) {
-//  alert ("You need to select at least one type of character");
-// return generatePassword()
-//}
+if (!specialChars && !numericalNums && !lowerChars && !upperChars) {
+ alert ("You need to select at least one type of character");
+return generatePassword()
+}
 
 //after validation is complete, start pushing the selected charcters into possible chars arays
 
-// if (specialChars) {
-  // possiblChars.concat(specialCharsArray)
-  // }
- // if (numericalNums ) {
-    // possibleChars.concat(numericalNumsArray)
-//}
+if (specialChars) {
+  possiblChars.concat(specialCharsArray)
+  }
+ if (numericalNums ) {
+    possibleChars.concat(numericalNumsArray)
+}
 //create a random number 
 
 // loop  through possibleChars and select a random character from the possibleCharsArray from based on how many charcters they entered into the prompt.
@@ -52,14 +55,13 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+ }
 
-}
-
-// Add event listener to generate button
+// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
